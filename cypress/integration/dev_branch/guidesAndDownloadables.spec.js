867f289e5dce352af3bc0cc8213a8dd5834aa947
Ricky Blondel <rickyblondel@icloud.com>
2018-06-11 17:40:09 +0400

    v1.1.110620181739
    Start from home page
    click on Guides in NavMenu

--- cypress/integration/dev_branch/guidesAndDownloadables.spec.js
+++ cypress/integration/dev_branch/guidesAndDownloadables.spec.js
@@ -9,11 +9,19 @@ describe('Test1 dev branch', function() {
         Cypress.on('uncaught:exception', (err, runnable) => {
             return false
         })
+        //resolution 
         cy.viewport(1366, 768)
-        cy.visit('https://author.pampers.com.pgsitecore.com/en-us/guides-and-downloadables')
+        //open pampers
+        cy.visit('https://author.pampers.com.pgsitecore.com/en-us/');
+        //verify Navigation menu
+        cy.get('.js-menu-list').contains('GUIDES').contains('New').click();
+        //verify breadcrumb
         cy.get('.c-breadcrumb').contains('Guides & Downloadables');
+        //verify hero top banner title
         cy.get('.hero-top-banner__title').contains('Guides & Downloadables');
+        //verify hero top banner text
         cy.get('.hero-top-banner__text').contains('These super-handy guides and downloadables will help you navigate through topics such as your pregnancy, your baby’s development, and so much more!');
+        //verify social title section
         cy.get('.section-social__title').contains('Do you know other parents who would like our Guides & Downloadables? Share this now:');
         //social icon section 
         cy.get('.js-share--facebook').contains('Facebook');
