0e9247c775c53bd63e641e951a5e9ae9ef3cac30
Ricky Blondel <rickyblondel@icloud.com>
2018-07-09 11:55:20 +0400

    dev.articlev2.090720181155
    screenShotMobile done

--- cypress/integration/dev_branch/articlev2.spec.js
+++ cypress/integration/dev_branch/articlev2.spec.js
@@ -20,54 +20,69 @@ describe('Screenshot', () => {
     let dateReport=new Date();
     let reportId=dateReport.getTime();
     let confVerifyArticleV2Element=dataFromJson.verifyArticleV2Element;
+    let confScreenShotMobile=dataFromJson.screenShotMobile;
     beforeEach(() => {
         Cypress.on('uncaught:exception', (err, runnable)=> {
             return false
         })
-        cy.viewport(1600, 1200);
     });
     for(var compt=0;compt<listUrls.length;compt++){
         let temp=compt;
         //open Url
         //check og title and url
-        it('TC'+temp+'-Check Page Info ::'+listUrls[temp][0]+'',()=>{
+        it('Open URL for :: '+listUrls[temp][0]+'',()=>{
+            cy.viewport(1600, 1200);
             cy.visit(listUrls[temp][1]);
-            cy.checkArticleV2PageInfo(listUrls[temp],reportId);
         });
         //take screenshot
         it('TC'+temp+'-0 Article V2 take screenshot ::'+listUrls[temp][0]+'',()=>{
             cy.checkVortexOpenAndTakeScreenShot('TC'+temp+' '+listUrls[temp][0]);
         });
         if(confVerifyArticleV2Element){
+            //verify page info
+            it('TC'+temp+'-1 Check Page Info ::'+listUrls[temp][0]+'',()=>{
+                cy.checkArticleV2PageInfo(listUrls[temp],reportId);
+            });
             //verify if element breadcumb contains itemptype data-vocabulary.org/breadcrumb
-            it('TC'+temp+'-1 Article V2 verify element braedcrumb vocabulary is present ::'+listUrls[temp][0]+'',()=>{
+            it('TC'+temp+'-2 Article V2 verify element braedcrumb vocabulary is present ::'+listUrls[temp][0]+'',()=>{
                 cy.checkArticleV2Breadcrumb();
             });
             //verify element title is present
-            it('TC'+temp+'-2 Article V2 verify element Title class is present ::'+listUrls[temp][0]+'',()=>{
+            it('TC'+temp+'-3 Article V2 verify element Title class is present ::'+listUrls[temp][0]+'',()=>{
                 cy.checkArticleV2Title(listUrls[temp][0]);
             });
             //verify element author name is present
             //both header and footer
-            it('TC'+temp+'-3 Article V2 verify data action detail for element author name ::'+listUrls[temp][0]+'',()=>{
+            it('TC'+temp+'-4 Article V2 verify data action detail for element author name ::'+listUrls[temp][0]+'',()=>{
                 cy.checkArticleV2AuthorName();
             });
             //verify element read time section is present
-            it('TC'+temp+'-4 Article V2 Verify CSS class for element date and time ::'+listUrls[temp][0]+'',()=>{
+            it('TC'+temp+'-5 Article V2 Verify CSS class for element date and time ::'+listUrls[temp][0]+'',()=>{
                 cy.checkArticleV2DateAndTimeInfo();
             });
             //verify element share and print is present
-            it('TC'+temp+'-5 Article V2 Verify both Share section and print icon are present ::'+listUrls[temp][0]+'',()=>{
+            it('TC'+temp+'-6 Article V2 Verify both Share section and print icon are present ::'+listUrls[temp][0]+'',()=>{
                 cy.checkArticleV2ShareSection();
             });
             //verift element like section is present
-            it('TC'+temp+'-6 Article V2 Verify element like icon is present ::'+listUrls[temp][0]+'',()=>{
+            it('TC'+temp+'-7 Article V2 Verify element like icon is present ::'+listUrls[temp][0]+'',()=>{
                 cy.checkArticleV2LikeIcon();
             });
             //verify element progressbar is present
             //check div.child(progress).should.max="100"
-            it('TC'+temp+'-7 Article V2 Verify element progressbar is present ::'+listUrls[temp][0]+'',()=>{
+            it('TC'+temp+'-8 Article V2 Verify element progressbar is present ::'+listUrls[temp][0]+'',()=>{
                 cy.checkArticleV2ProgressBar();
+            });
+        }
+    }
+    for(var compt=0;compt<listUrls.length;compt++){
+        let temp=compt;
+        //take screenshot on mobile
+        if(confScreenShotMobile){
+            it('MobileVersion-'+temp+' :: '+listUrls[temp][0]+'',()=>{
+                cy.viewport(320, 480);
+                cy.visit(listUrls[temp][1]);
+                cy.checkVortexOpenAndTakeScreenShot('MobileVersion-'+temp+'-'+listUrls[temp][0]+'');
             });
         }
     }
