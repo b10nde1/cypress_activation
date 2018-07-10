3d0916cd2fe0440930cb8e58a5e9897a5609f00a
Ricky Blondel <rickyblondel@icloud.com>
2018-07-10 15:30:00 +0400

    dev.integration.outbrainv2
    outbrainv2 done

--- cypress/integration/activation/outbrainV2/outbrain.js
+++ cypress/integration/activation/outbrainV2/outbrain.js
@@ -1,4 +1,4 @@
-import dataFromJson from '../../fixtures/data/outbrainv2.json';
+import dataFromJson from '../../../fixtures/data/outbrainv2.json';
 describe('Outbrain V2',()=>{
     const alignTableFromJson=(argSplit)=>{
         let result=new Array(argSplit.length);
@@ -35,34 +35,26 @@ describe('Outbrain V2',()=>{
     });
     for(var compt=0;compt<listUrls.length;compt++){
         let temp=compt;
-        //open Url
-        //check og title and url
         it('Open URL for :: '+listUrls[temp][0]+'',()=>{
             cy.visit(listUrls[temp][1]);
         });
-        //take screenshot
         it('TC'+temp+'-0 Outbrain V2 take screenshot ::'+listUrls[temp][0]+'',()=>{
             cy.wait(6000);
             cy.checkVortexOpenAndTakeScreenShot('TC'+temp+'-OutbrainV2-'+listUrls[temp][0],'.c-suggestion-v2.js-outbrain-v2');
         });
-        //verify 3 closed box are present
         it('TC'+temp+'-1 Outbrain V2 verify element closed box ::'+listUrls[temp][0]+'',()=>{
             cy.checkOutbrainV2ClosedBox();
         });
-        //verify footer text
         it('TC'+temp+'-2 Outbrain V2 verify footer text ::'+listUrls[temp][0]+'',()=>{
             cy.checkOutbrainV2FooterText(confRecommendedByText);
         });
-        //verify Outbrain Popin
         it('TC'+temp+'-3 Outbrain V2 verify Popin ::'+listUrls[temp][0]+'',()=>{
             cy.checkOutbrainV2Popin(confRecommendedByText);
         });
-        //Verify h1 for outbrain section 
         it('TC'+temp+'-4 Outbrain V2 verify title is present ::'+listUrls[temp][0]+'',()=>{
             cy.checkOutbrainV2H2Title(confH2Title);
         });
     }
-    //screenshot on mobiles
     if(confScreenShotMobile){
         try{
             if(listUrls===null)throw("Error || confScreenShotMobile :: ListUrls is null");
@@ -80,7 +72,6 @@ describe('Outbrain V2',()=>{
             console.log("Error || confScreenShotMobile === true ::"+ex);
         }
     }
-    //report json for screenshot with tcid+article name + url
     it('Report id :: OutbrainV2id'+reportId+'',()=>{
         cy.checkGlobalScreenShotReport('outbrainV2',listUrls,reportId);
     });
