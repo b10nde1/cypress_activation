930f87e7ec223dff1fa2973d6e7cd552c9ec4f5a
Ricky Blondel <rickyblondel@icloud.com>
2018-07-19 11:58:00 +0400

    dev.kraken.190720181157
    checkGlobalScreenShotReport

--- cypress/integration/dev_branch/dev.kraken.spec.js
+++ cypress/integration/dev_branch/dev.kraken.spec.js
@@ -19,7 +19,7 @@ describe('Screenshot', () => {
     let confDevice=getTable2DFromJson(dataFromJson.device.split('],['),',');
     let listMarkets=getTable2DFromJson(dataFromJson.urls.split('],['),'/*/');
     let confGetStatusCodeReport=dataFromJson.getStatusCodeReport;
-    let reportId=new Date();
+    let reportDate=new Date(); let reportId=reportDate.getTime();
     for(var comptDevice=0;comptDevice<confDevice.length;comptDevice++){
         let confWidth=Number(confDevice[comptDevice][0]);
         let confHeight=Number(confDevice[comptDevice][1]);
@@ -37,9 +37,12 @@ describe('Screenshot', () => {
             });
         }
     }
+    if('Kraken | Get ScreenShot report '+reportId+' ',()=>{
+        cy.checkGlobalScreenShotReport('kraken-screenShotReport',listMarkets,reportId);
+    });
     if(confGetStatusCodeReport){
-        it('Kraken | Get Status Code report '+reportId.getTime()+'',()=>{
-            cy.checkUtilGetStatusCodeReport('kraken-statusCodeReport',listMarkets,reportId.getTime());
+        it('Kraken | Get Status Code report '+reportId+'',()=>{
+            cy.checkUtilGetStatusCodeReport('kraken-statusCodeReport',listMarkets,reportId);
         })
     }
 })
