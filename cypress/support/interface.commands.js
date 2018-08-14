000ee6d01bb7b974851099adcfc1374dca52efbf
Ricky Blondel <rickyblondel@icloud.com>
2018-08-14 12:26:00 +0400

    dev.report.140820181225
    interface.commands.js
    -createBalise()
    -createHtml()
    
    +npm install flow-bin

--- cypress/support/interface.commands.js
+++ cypress/support/interface.commands.js
@@ -7,6 +7,95 @@ const writeFile=(argFileTitle,argReportId,argFileExt,argData)=>{
     }
 }
 
+const getBaliseOption=(argOption)=>{
+    try{
+        //for option -> 
+        //[0] = open or close '/' if close
+        //[1] = class or id
+        //[2] = class or id value
+        let option=['','',''];
+        for(var compt=0;compt<3;compt++){
+            if(argOption[compt]!=null)option[compt]=argOption[compt]
+        }
+        return option;
+    }
+    catch(ex){
+        cy.checkUtilConsole(['interface commands -> getBaliseOption'],[ex]);
+    }
+}
+
+//['div','p','p','div']+[['','class','test'],['','',''],['/','',''],['/','','']]+true+true => <div class="test"><p></p></div>
+//['div','p','p','div']+[['','class','test'],['','',''],['/','',''],['/','','']]+true+false => ['<div class="test">','<p>','</p>','</div>']
+//['div']+['','','']+false+true => <div></div>
+//['div']+['','','']+false+false => ['<div>','</div>']
+const createBalise=(argListBalise,argOption,argMultipleOption,argAligned)=>{
+    try{
+        let option=null;let resultAligned='';let resultNotAligned=new Array(argListBalise.length);
+        //case argMultipleOption true
+        if(argMultipleOption){
+            let tableOfOption=new Array(argOption.length);
+            for(var compt=0;compt<argOption.length;compt++){
+                tableOfOption[compt]=new Array(3);
+                tableOfOption[compt]=getBaliseOption(argOption[compt]);
+            }
+            for(var compt=0;compt<argListBalise.length;compt++){    
+                option=getBaliseOption(tableOfOption[compt]);
+                if(argAligned)resultAligned+='<'+option[0]+''+argListBalise[compt]+' '+option[1]+'="'+option[0]+'">'
+                else resultNotAligned[compt]='<'+option[0]+''+argListBalise[compt]+' '+option[1]+'="'+option[0]+'">';
+            }
+        }
+        //case argMultipleOption false
+        else {
+            option=getBaliseOption(argOption);
+            for(var compt;compt<argListBalise.length;compt++){
+                if(argAligned)resultAligned+='<'+option[0]+''+argListBalise[compt]+' '+option[1]+'="'+option[0]+'">';
+                else resultNotAligned[compt]='<'+option[0]+''+argListBalise[compt]+' '+option[1]+'="'+option[0]+'">';
+            }
+        }
+        if(argAligned)return resultAligned;
+        else return resultNotAligned;
+        throw('interface commands -> closeBalise : ERROR NO RESULT');
+    }
+    catch(ex){
+        cy.checkUtilConsole(['interface commands -> closeBalise'],[ex]);
+    }
+}
+
+const createHtml=(argTitle,argBody,argCss,argJs)=>{
+/* 
+    <html>
+        <head>
+            <title></title>
+            <style></style>
+            <script></script>
+        </head>
+        <body>
+
+        </body>
+    </html>
+*/
+    try{
+        let listBalise=createBalise(
+            ['html','html','head','head','body','body','title','title','style','style','script','script']
+            ,[['','',''],['/','',''],['','',''],['/','',''],['','',''],['/','',''],['','',''],['/','',''],['','',''],['/','',''],['','',''],['/','','']]
+            ,true
+            ,false);
+        return listBalise[0]
+                    +listBalise[2]
+                        +listBalise[6]+argTitle+listBalise[7]
+                        +listBalise[8]+argCss+listBalise[9]
+                        +listBalise[10]+argJs+listBalise[11]
+                    +listBalise[3]
+                    +listBalise[4]
+                        +argBody
+                    +listBalise[5]
+                +listBalise[1];
+    }
+    catch(ex){
+        cy.checkUtilConsole(['interface commands -> createHtml'],[ex]);
+    }
+}
+
 Cypress.Commands.add('interfaceStatusCodeReport',(argTitle,argReportId,argHeaderSection,argDataSection)=>{
     try{
         let extractHeaderSection='';let extractDataSection=new Array(argDataSection.length);
@@ -14,7 +103,12 @@ Cypress.Commands.add('interfaceStatusCodeReport',(argTitle,argReportId,argHeader
             let tempSplit=argDataSection[compt].split('/*/');
             let tempColor='green';
             if(tempSplit[0]!=200) tempColor='red'
-            extractDataSection[compt]='<div id='+tempSplit[1]+'><button style="color:'+tempColor+'">'+tempSplit[0]+'</button> <a href="'+tempSplit[2]+'">Item : '+tempSplit[1]+'</a></div>';
+            extractDataSection[compt]='<div id='
+                +tempSplit[1]+'><button style="color:'
+                +tempColor+'">'
+                +tempSplit[0]+'</button> <a href="'
+                +tempSplit[2]+'">Item : '
+                +tempSplit[1]+'</a></div>';
         }
         for(var compt=0;compt<argHeaderSection[0].length;compt++){
             extractHeaderSection+='<div id='+argHeaderSection[0][compt]+'><p>'+argHeaderSection[0][compt]+' :: '+argHeaderSection[1][compt]+'</p></div>';
