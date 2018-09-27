607ad30828afc968f6e64c7bf3e0d7b8e81380a2
Ricky Blondel <rickyblondel@icloud.com>
2018-09-27 10:50:42 +0400

    fix.xml
    unable to access Stage environment from Cypress (VPN issue)
    Note : can download sitemap.xml
    Fix for UI with the text OK>' in result

--- cypress/support/interface.commands.js
+++ cypress/support/interface.commands.js
@@ -226,9 +226,8 @@ Cypress.Commands.add('interfaceSitemapReport',(argModule: string,argReportId: Da
         let extractData=new Array(argData.length);
         for(var compt=0;compt<argData.length;compt++){
             extractData[compt]=new Array(3);
-            let tempData=argData[compt].split('/*/'); let tempColor='blue';
-            if(tempData[0]!='OK')tempColor='silver';
-            extractData[compt][0]='<button style="color=white;background-color:'+tempColor+'">'+tempData[0]+'</button>';
+            let tempData=argData[compt].split('/*/');
+            extractData[compt][0]=tempData[0];
             extractData[compt][1]=tempData[1];
             extractData[compt][2]=tempData[2];
         }
